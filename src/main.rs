e822c3dda40afcb2788b4ed008e6d32a50bb5e2e
gabriel.leonardo.cruz2012@gmail.com <gabriel.leonardo.cruz2012@gmail.com>
2025-08-10 13:31:02 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use dioxus::prelude::*;
+use fastrand;
+
+#[allow(non_snake_case)]
+fn main() {
+    launch(app);
+}
+
+#[component]
+fn app() -> Element {
+    // Declare como mut!
+    let mut senha = use_signal(|| String::new());
+
+    rsx! {
+        div {
+            style: "
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                gap: 20px;
+                font-family: monospace;
+            ",
+            button {
+                style: "
+                    background-color: #0984ffff;
+                    color: white;
+                    border: none;
+                    padding: 10px 20px;
+                    font-size: 16px;
+                    border-radius: 8px;
+                    cursor: pointer;
+                    transition: background-color 0.3s ease;
+                ",
+                onclick: move |_| {
+                    let nova_senha = (0..12)
+                        .map(|_| (fastrand::u8(33..127)) as char)
+                        .collect::<String>();
+                    senha.write().replace_range(.., &nova_senha);
+                },
+                "🔐 Gerar Senha"
+            },
+
+            h1 {
+                style: "
+                    font-size: 24px;
+                    color: #000000ff;
+                ",
+                "Senha Gerada:"
+            },
+
+            h2 { "{senha.read()}" }
+        }
+    }
+}
